102c0134bf22da11539a0202744acd6f522eeda4
Ievgen Bardadym <ievgen.bardadym@xtendx>
2023-12-25 10:12:05 +0100

    fix(build): merge isssue

--- src/main/java/currency/impl/NumSignsButtonHandler.java
+++ src/main/java/currency/impl/NumSignsButtonHandler.java
@@ -5,7 +5,6 @@ import currency.Handlers;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
